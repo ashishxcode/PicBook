9c24a02afab71ac3b73f348f67afe87f84f72b80
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-25 19:14:56 +0530

    updated : styles
    - added gradient border to images
    - displayed the images in grid view
    - used media queries to handle smaller resolutions breakpoints

--- client/src/Components/ImageGrid.js
+++ client/src/Components/ImageGrid.js
@@ -7,27 +7,30 @@ const ImageGrid = ({ selectedImage, setSelectedImage }) => {
 
 	console.log("Docs --->", docs);
 	return (
-		<div className="img-grid">
-			{docs &&
-				docs.map((item) => {
-					return (
-						<motion.div
-							className="img-wrap"
-							layout
-							whileHover={{ opacity: 1 }}
-							key={item.id}
-							onClick={() => setSelectedImage(item.url)}
-						>
-							<motion.img
-								src={item.url}
-								alt="Oops"
-								initial={{ opacity: 0 }}
-								animate={{ opacity: 1 }}
-								delay={{ delay: 1 }}
-							/>
-						</motion.div>
-					);
-				})}
+		<div className="masonry-wrapper">
+			<div className="masonry">
+				{docs &&
+					docs.map((item) => {
+						return (
+							<motion.div
+								className="masonry-item"
+								layout
+								whileHover={{ opacity: 1 }}
+								key={item.id}
+								onClick={() => setSelectedImage(item.url)}
+							>
+								<motion.img
+									src={item.url}
+									alt="Oops"
+									initial={{ opacity: 0 }}
+									animate={{ opacity: 1 }}
+									delay={{ delay: 1 }}
+									className="masonry-content"
+								/>
+							</motion.div>
+						);
+					})}
+			</div>
 		</div>
 	);
 };
