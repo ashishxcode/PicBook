d4608dec804cf8c1e2612894637bc04325613fd5
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-25 22:06:11 +0530

    Update : App.js
    - edited tagline

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,16 @@ import ImageGrid from "./Components/ImageGrid";
 import Modal from "./Components/Modal";
 import UploadFile from "./Components/UploadFile";
 
-
 function App() {
 	const [selectedImage, setSelectedImage] = useState(null);
 	return (
-		<div className="App">		
-		<header className="header">
-			<div className="brand">
-				<h1 className="brand__name">PicBook</h1>
-				<small className="brand__tagline">Adding Moments of life.</small>
-			</div>
-		</header>
+		<div className="App">
+			<header className="header">
+				<div className="brand">
+					<h1 className="brand__name">PicBook</h1>
+					<small className="brand__tagline">Adding Life To Moments.</small>
+				</div>
+			</header>
 			<UploadFile />
 			<ImageGrid
 				selectedImage={selectedImage}
