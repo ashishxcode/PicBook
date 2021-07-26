ee396ecdf530040c515be4deb06239a978b1b5e2
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-26 17:50:40 +0530

    fix : App.js
    - ToastContaianer

--- client/src/App.js
+++ client/src/App.js
@@ -13,19 +13,17 @@ function App() {
 	console.log("CHECK-->", process.env.REACT_APP_FIREBASE_API_KEY);
 	return (
 		<div className="App">
-			<ToastContainer>
-				<header className="header">
-					<div className="brand">
-						<h1 className="brand__name">PicBook</h1>
-						<small className="brand__tagline">Adding Life To Moments.</small>
-					</div>
-				</header>
-
-				<Switch>
-					<Route path="/" exact component={Welcome} />
-					<Route path="/user" exact component={UserFeed} />
-				</Switch>
-			</ToastContainer>
+			<header className="header">
+				<div className="brand">
+					<h1 className="brand__name">PicBook</h1>
+					<small className="brand__tagline">Adding Life To Moments.</small>
+				</div>
+			</header>
+			<ToastContainer />
+			<Switch>
+				<Route path="/" exact component={Welcome} />
+				<Route path="/user" exact component={UserFeed} />
+			</Switch>
 		</div>
 	);
 }
