eac2dbb052f0adacfd7b43040f1f21dbe280765b
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-26 17:17:28 +0530

    create : userReducer
    - reducer function to handle user log state
    
    - exported the reducer

---
+++ client/src/Reducers/userReducer.js
@@ -0,0 +1,12 @@
+export const userReducer = (state = null, action) => {
+	switch (action.type) {
+		default:
+			return state;
+
+		case "USER_LOGGED_IN":
+			return action.payload;
+
+		case "USER_LOGGED_OUT":
+			return action.payload;
+	}
+};
