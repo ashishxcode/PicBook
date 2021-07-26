41d799c81a67410b305522b0a22c8d31c569871b
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-26 17:30:38 +0530

    Update : App.js
    - imported react-toastify container and CSS for application

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,9 @@
 import { useState } from "react";
 
+//toast
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 import { Path, Route, Switch } from "react-router-dom";
 import UserFeed from "./Components/User/UserFeed";
 import Welcome from "./Components/welcome";
@@ -9,16 +13,19 @@ function App() {
 	console.log("CHECK-->", process.env.REACT_APP_FIREBASE_API_KEY);
 	return (
 		<div className="App">
-			<header className="header">
-				<div className="brand">
-					<h1 className="brand__name">PicBook</h1>
-					<small className="brand__tagline">Adding Life To Moments.</small>
-				</div>
-			</header>
-			<Switch>
-				<Route path="/" exact component={Welcome} />
-				<Route path="/user" exact component={UserFeed} />
-			</Switch>
+			<ToastContainer>
+				<header className="header">
+					<div className="brand">
+						<h1 className="brand__name">PicBook</h1>
+						<small className="brand__tagline">Adding Life To Moments.</small>
+					</div>
+				</header>
+
+				<Switch>
+					<Route path="/" exact component={Welcome} />
+					<Route path="/user" exact component={UserFeed} />
+				</Switch>
+			</ToastContainer>
 		</div>
 	);
 }
