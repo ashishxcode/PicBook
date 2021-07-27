4cd82f5a3eb393d5b2ae899f05c670cafb1f168c
ashishxcode <ashish.patel0903@gmail.com>
2021-07-27 11:53:06 +0530

    Added UserProfile Photo on a Database

--- client/src/App.js
+++ client/src/App.js
@@ -5,23 +5,31 @@ import "react-toastify/dist/ReactToastify.css";
 
 import {  Route, Switch } from "react-router-dom";
 import UserFeed from "./Components/User/UserFeed";
-import {auth} from "./firebase/config";	
+import {auth,projectFirestore} from "./firebase/config";	
 import Welcome from "./Components/welcome";
 import { useDispatch } from 'react-redux';
         
 function App() {
 	const [userState, setUserState] = useState(null); 
 	const dispatch = useDispatch();
-
+	const collectionRef = projectFirestore.collection("users");
 	useEffect(() => {
 		auth.onAuthStateChanged(async (user) => {
 		 if(user){
+			 
+			await collectionRef.add({
+				 'Name': user.displayName,
+				 'Email':user.email, 
+				 "UserProfile":user.photoURL
+			});
+
 			 console.log("Firebase User-->",user);
 			 dispatch({	
 				 		type: "USER_LOGGED_IN",
 						payload: {
 							Name: user.displayName,
 							Email: user.email,
+							UserProfile: user.photoURL
 						}
 					});
 			setUserState(user);
@@ -29,7 +37,7 @@ function App() {
 			setUserState(null);
 		 }
 		});
-	} , [dispatch]);
+	} , [dispatch,collectionRef]);
 
 	const handleLogout = () => {
 		auth.signOut();
--- client/src/Components/welcome.js
+++ client/src/Components/welcome.js
@@ -1,34 +1,21 @@
 import React from "react";
-import { auth, googleAuthProvider,projectFirestore } from "../firebase/config";
+import { auth, googleAuthProvider } from "../firebase/config";
 import { toast } from "react-toastify";
-import { useDispatch } from "react-redux";
 
 const Welcome = ({ history }) => {
-	const dispatch = useDispatch();
-	const collectionRef = projectFirestore.collection("users");
 	const userSignUp = () => {
 		console.log("Working");
 
 		auth
 			.signInWithPopup(googleAuthProvider)
 			.then(async (result) =>  {
-				var userCreds = result.credential;
-				// var user = auth.currentUser;
 				const user = result.user;
 				const token = user.getIdTokenResult();
 				console.log("Login Was done, user info--->", user);
 				console.log("Token-->", token);
 				toast.success("Logged In Succesfully!");
 				if (user.emailVerified){
-					await collectionRef.add({'name': user.displayName,'email':user.email});
 					history.push("/user");
-					dispatch({
-						type: "USER_LOGGED_IN",
-						payload: {
-							Name: user.displayName,
-							Email: user.email,
-						},
-					});
 				}
 			})
 			.catch((err) => console.log(err));
@@ -36,7 +23,6 @@ const Welcome = ({ history }) => {
 	return (
 		<div>
 			<h2 className="test">Welcome To PicBook</h2>
-
 			<button onClick={userSignUp}>Sign Up With Google</button>
 		</div>
 	);
