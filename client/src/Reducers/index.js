e970b06b17cd9e6ff33221967f4e89ee326f097a
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-28 11:43:20 +0530

    update : index.js
    - imported and used the modalreducer in the combineReducer method

--- client/src/Reducers/index.js
+++ client/src/Reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import { userReducer } from "./userReducer";
+import { ModalReducer } from "./modalReducer";
 
 export const rootReducer = combineReducers({
 	user: userReducer,
+	modal: ModalReducer,
 });
