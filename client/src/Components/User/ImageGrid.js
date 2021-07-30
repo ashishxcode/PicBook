42ac1d2fc0ead9708ba1f27b23dc7cf43e3146ca
ashishxcode <ashish.patel0903@gmail.com>
2021-07-30 19:18:20 +0530

    Added stye on ImageGrid with action button

--- client/src/Components/User/ImageGrid.js
+++ client/src/Components/User/ImageGrid.js
@@ -1,21 +1,33 @@
 import React from 'react';
 import useFirestore from '../../Hooks/useFIrestore';
 import { motion } from 'framer-motion';
-import {
-	FacebookShareButton,
-	FacebookIcon,
-	TwitterShareButton,
-	TwitterIcon,
-	WhatsappShareButton,
-	WhatsappIcon,
-	PinterestShareButton,
-	PinterestIcon,
-} from 'react-share';
-import { FiDownload } from 'react-icons/fi';
+import { GoKebabHorizontal } from 'react-icons/go';
+import { Menu, Dropdown } from 'antd';
+import 'antd/dist/antd.css';
 
 const ImageGrid = ({ selectedImage, setSelectedImage }) => {
 	const { docs } = useFirestore('Images');
 
+	const menu = (
+		<Menu>
+			<Menu.Item>
+				<a
+					rel="noopener noreferrer"
+					href="/path/to/image"
+					download="Image URL">
+					Download
+				</a>
+			</Menu.Item>
+			<Menu.Item>
+				<a
+					target="_blank"
+					rel="noopener noreferrer"
+					href="https://www.luohanacademy.com">
+					Share
+				</a>
+			</Menu.Item>
+		</Menu>
+	);
 	return (
 		<div className="masonry">
 			{docs &&
@@ -32,49 +44,13 @@ const ImageGrid = ({ selectedImage, setSelectedImage }) => {
 									alt="Oops"
 									onClick={() => setSelectedImage(item.url)}
 								/>
-								<div className="card__action">
-									<div className="share-buttons">
-										<FacebookShareButton
-											quote={'Check out my pic - PicBook'}
-											media={item.url}
-											url={item.url}>
-											<FacebookIcon
-												size={24}
-												round={true}
-											/>
-										</FacebookShareButton>
-										<TwitterShareButton
-											quote={'Check out my pic - PicBook'}
-											media={item.url}
-											url={item.url}>
-											<TwitterIcon
-												size={24}
-												round={true}
-											/>
-										</TwitterShareButton>
-										<PinterestShareButton
-											quote={'Check out my pic - PicBook'}
-											media={item.url}
-											url={item.url}>
-											<PinterestIcon
-												size={24}
-												round={true}
-											/>
-										</PinterestShareButton>
-										<WhatsappShareButton
-											quote={'Check out my pic - PicBook'}
-											media={item.url}
-											url={item.url}>
-											<WhatsappIcon
-												size={24}
-												round={true}
-											/>
-										</WhatsappShareButton>
-									</div>
-									<a href={`/{item.url}`} download>
-										<FiDownload className="nav__icon" />
-									</a>
-								</div>
+								<Dropdown
+									className="masonry-item-menu"
+									overlay={menu}>
+									<span onClick={(e) => e.preventDefault()}>
+										<GoKebabHorizontal className="menu__icon" />
+									</span>
+								</Dropdown>
 							</div>
 						</motion.div>
 					);
